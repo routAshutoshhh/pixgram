1540b218b23460319766b374dd07f10b2b96d43a
Ashutosh Kumar Rout <129250384+routAshutoshhh@users.noreply.github.com>
2025-03-30 15:21:12 +0530

    some basic setup for the pages and overall layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <h1 className="text-5xl font-bold underline">Hello Pixgram</h1>;
+  return (
+    <main className="flex h-screen">
+      {/*wrapping in parent route */}
+      <Routes>
+        {/*public route : every one will be able to see this route like signup and signin*/}
+        <Route path="/sign-in" element={<SigninForm />} />
+        {/*private route:only can be seen if singned in */}
+        <Route index element={<Home />} />
+        {/* index when used first/starting page to appear  */}
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
